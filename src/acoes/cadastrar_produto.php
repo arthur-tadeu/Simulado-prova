904948ef9d055fdd86985051ce2b3958e3d53b65
arthur-tadeu <arthur.tadeu.carvalhoo@gmail.com>
2026-04-27 16:08:33 -0300

    correcao simplificacao de campos

--- src/acoes/cadastrar_produto.php
+++ src/acoes/cadastrar_produto.php
@@ -4,10 +4,10 @@ require_once("../../conection.php");
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
     $nome = $_POST["nome"];
-    $especificacoes = $_POST["especificacoes"];
     $estoque_minimo = $_POST["estoque_minimo"];
 
-    $sql = "INSERT INTO produtos (nome, especificacoes, estoque_minimo, estoque_atual) VALUES ('$nome', '$especificacoes', $estoque_minimo, 0)";
+    // Removido 'especificacoes' para simplificar o sistema
+    $sql = "INSERT INTO produtos (nome, estoque_minimo, estoque_atual) VALUES ('$nome', $estoque_minimo, 0)";
     
     if(mysqli_query($con, $sql)){
         header("Location: ../paginas/cadastro_produto.php");
